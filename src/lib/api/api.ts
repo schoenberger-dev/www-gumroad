0a37d604e55c83719ff96cd805c9dab92eefcb58
niklas schoenberger <001@schoenberger.dev>
2024-06-11 03:14:52 +0200

    fix: api fetch urls

--- src/lib/api/api.ts
+++ src/lib/api/api.ts
@@ -8,7 +8,7 @@ export default class API {
     );
 
     return await fetch(
-      `${process.env.NEXT_PUBLIC_BACKEND_API_URL}${url}`,
+      `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/${url}`,
       init,
     );
   }
