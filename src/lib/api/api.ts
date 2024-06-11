cca8ed220d6e4e03c3afa06def69231492d9ca6a
niklas schoenberger <001@schoenberger.dev>
2024-06-11 03:15:52 +0200

    fix: api fetch urls

--- src/lib/api/api.ts
+++ src/lib/api/api.ts
@@ -1,14 +1,14 @@
 export default class API {
   static token = process.env.API_TOKEN;
 
-  static async GET(url: string | URL | Request, options?: RequestInit) {
+  static async GET(resourceUrl: string | URL | Request, options?: RequestInit) {
     const init = Object.assign(
       { method: 'GET', headers: { Authorization: API.token } },
       options,
     );
 
     return await fetch(
-      `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/${url}`,
+      `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/${resourceUrl}`,
       init,
     );
   }
