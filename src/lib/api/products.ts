98c4d193454d4b7dfb8b23a81a286edd8e1601c5
niklas schoenberger <001@schoenberger.dev>
2024-06-11 03:20:18 +0200

    fix: api fetch urls

--- src/lib/api/products.ts
+++ src/lib/api/products.ts
@@ -1,17 +1,12 @@
 import API from './api';
 
 export async function getAll(): Promise<Product[]> {
-  try {
-    const res = await API.GET(
-      `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/products`,
-      {
-        cache: 'no-store',
-      },
-    );
+  const res = await API.GET(
+    `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/products`,
+    {
+      cache: 'no-store',
+    },
+  );
 
-    return await res.json();
-  } catch (error: any) {
-    console.error('Could not fetch products', error);
-    throw new Error(error);
-  }
+  return await res.json();
 }
