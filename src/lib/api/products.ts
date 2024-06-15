eb16f17ea4639776b38211bf8f2a8a0c12214523
niklas schoenberger <001@schoenberger.dev>
2024-06-15 02:29:06 +0200

    fix: products api methods

--- src/lib/api/products.ts
+++ src/lib/api/products.ts
@@ -3,17 +3,27 @@
 import { API } from './api';
 
 export async function getAll(): Promise<Product[]> {
-  const res = await API.GET('products', {
-    cache: 'no-store',
-  });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/products`,
+    {
+      method: 'GET',
+      headers: { Authorization: `${process.env.API_TOKEN}` },
+      cache: 'no-store',
+    },
+  );
 
   return await res.json();
 }
 
 export async function search(query: string): Promise<SearchResults> {
-  const res = await API.GET(`products/search?q=${query}`, {
-    cache: 'no-store',
-  });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/products/search?q=${query}`,
+    {
+      method: 'GET',
+      headers: { Authorization: `${process.env.API_TOKEN}` },
+      cache: 'no-store',
+    },
+  );
 
   console.log(res.statusText);
 
